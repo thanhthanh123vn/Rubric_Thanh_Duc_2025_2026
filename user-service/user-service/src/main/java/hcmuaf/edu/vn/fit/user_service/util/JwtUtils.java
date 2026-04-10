782ac84dc25a5a63a9750172a5fafa022b810068
VanDuc <22130050@st.hcmuaf.edu.vn>
2026-04-10 15:52:48 +0700

    update jwtFilter and fetch api user to course

--- user-service/user-service/src/main/java/hcmuaf/edu/vn/fit/user_service/util/JwtUtils.java
+++ user-service/user-service/src/main/java/hcmuaf/edu/vn/fit/user_service/util/JwtUtils.java
@@ -1,10 +1,13 @@
 package hcmuaf.edu.vn.fit.user_service.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -13,14 +16,43 @@ public class JwtUtils {
     private  String SECRET_KEY ;
     private final long EXPIRATION_TIME = 86400000;
 
+    public Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+
     public String generateToken(String userId, String role) {
         return Jwts.builder()
                 .setSubject(userId)
                 .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
+                .signWith(getSigningKey())
+                .
+                compact();
     }
 
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class);
+    }
+    public boolean isTokenValid(String token) {
+        try{
+            Claims claims = extractAllClaims(token);
+
+            return claims.getExpiration().after(new Date());
+        }catch (Exception e){
+
+        }
+        return false;
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
