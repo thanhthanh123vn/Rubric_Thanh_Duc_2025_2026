1f9759ec81b9f369c11009e761133da7f6591aa8
VanDuc <22130050@st.hcmuaf.edu.vn>
2026-04-23 11:56:57 +0700

    add config AWS3 for upload file

---
+++ course-service/course-service/src/main/java/hcmuaf/edu/vn/fit/course_service/service/S3Service.java
@@ -0,0 +1,42 @@
+package hcmuaf.edu.vn.fit.course_service.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+    @Value("${aws.bucketName}")
+    private String bucketName;
+
+    private final S3Client s3Client;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+
+        String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(
+                request,
+                RequestBody.fromBytes(file.getBytes())
+        );
+
+        // URL public
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+    }
+}
